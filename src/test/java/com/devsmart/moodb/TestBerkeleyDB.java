60f4d52e28e380447027df89ab506e9d59a1d681
Paul Soucy <paul@diskless.(none)>
2014-08-12 19:59:05 -0400

    initial

---
+++ src/test/java/com/devsmart/moodb/TestBerkeleyDB.java
@@ -0,0 +1,41 @@
+package com.devsmart.moodb;
+
+import com.sleepycat.je.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+public class TestBerkeleyDB {
+
+    @Test
+    public void testDB() throws Exception {
+
+
+        File baseDir = new File("data/env1");
+        baseDir.mkdirs();
+
+        EnvironmentConfig envConfig = new EnvironmentConfig();
+        envConfig.setAllowCreate(true);
+        Environment myDbEnvironment = new Environment(baseDir, envConfig);
+
+        DatabaseConfig dbConfig = new DatabaseConfig();
+        dbConfig.setAllowCreate(true);
+        Database myDatabase = myDbEnvironment.openDatabase(null, "sampleDatabase", dbConfig);
+
+
+        DatabaseEntry theKey = new DatabaseEntry("neat".getBytes("UTF-8"));
+        DatabaseEntry theValue = new DatabaseEntry("value".getBytes("UTF-8"));
+        myDatabase.put(null, theKey, theValue);
+
+        DatabaseEntry data = new DatabaseEntry();
+        assertTrue(myDatabase.get(null, theKey, data, LockMode.DEFAULT) == OperationStatus.SUCCESS);
+        assertEquals("value", new String(data.getData(), "UTF-8"));
+
+
+
+        myDatabase.close();
+
+
+    }
+}
